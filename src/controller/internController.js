0ac9d1e4f94089908902d0915ac6d24d61375f10
harsh -uranium <harshshrivastav139@gmail.com>
2022-05-06 11:31:31 +0530

    project 2

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -7,6 +7,7 @@ let isValid = (value) => {
     if (typeof value === "string" && value.trim().length === 0) return false;
     return true;
 };
+let isValidObjectId = function(objectId) { return mongoose.Types.ObjectId.isValid(objectId) }
 
 //======================= Create Intern Document====================//
 
